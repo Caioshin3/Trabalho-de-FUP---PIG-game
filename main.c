8fe01ec264d7ec4bee5f6b0effcc263993ea4396
Caio Oliveira <133588078+Caioshin3@users.noreply.github.com>
2023-12-08 20:38:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include "lib.h"
 
+// Função principal que inicia o programa
 int main()
 {
+    // Chama a função para exibir o menu principal
     Menu();
-    return 0;    
+
+    // Retorna 0 para indicar que o programa foi executado sem erros
+    return 0;
 }
